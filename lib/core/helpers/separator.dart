9cba7ade08c454a2ed817acef5f0a310807dbb07
karim_slama <karimslama917@gmail.com>
2024-10-15 23:43:34 +0300

    - Create Book An Appointment Screen and finish its UI correctly without conflict - Added Some variables in App String file to be suitable to the design and go away from hardcoded - Modify App Routing file

---
+++ lib/core/helpers/separator.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:si_sehat/core/theming/app_colors/app_colors.dart';
+
+Widget separator() => Container(
+      width: double.infinity,
+      height: 1,
+      color: AppColors.grey,
+    );
